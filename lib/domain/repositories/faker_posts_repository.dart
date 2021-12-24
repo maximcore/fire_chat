5f299fbd508913c6c332433b132bfc9732eb2bb8
Maxim Borisenko <maximborisenko7@gmail.com>
2021-12-24 10:24:36 -0800

    Fixes in faker_posts_repository.dart

--- lib/domain/repositories/faker_posts_repository.dart
+++ lib/domain/repositories/faker_posts_repository.dart
@@ -3,23 +3,20 @@ import 'package:fire_chat/domain/entities/post_entity/post_entity.dart';
 import 'package:fire_chat/domain/repositories/posts_repository.dart';
 
 class FakerPostsRepository implements PostsRepository {
-  final int maxDescriptionLength = 100;
+  static const int _maxDescriptionLength = 100;
+  static const int _postsAmount = 100;
 
   @override
   Future<List<PostEntity>> fetchPosts() async {
     await Future<void>.delayed(const Duration(seconds: 3));
-    final list = <PostEntity>[];
-    for (var i = 0; i < maxDescriptionLength; i++) {
-      list.add(
-        PostEntity(
-          description: faker.randomGenerator.string(
-            maxDescriptionLength,
-            min: maxDescriptionLength,
-          ),
-          username: faker.internet.userName(),
-        ),
-      );
-    }
+    final list = List<PostEntity>.generate(
+      _postsAmount,
+      (_) => PostEntity(
+        description: faker.randomGenerator
+            .string(_maxDescriptionLength, min: _maxDescriptionLength),
+        username: faker.internet.userName(),
+      ),
+    );
     return list;
   }
 }
