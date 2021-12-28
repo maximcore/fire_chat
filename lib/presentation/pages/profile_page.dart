157a80c99c1c7660cd51512eadc68874308f5294
Maxim Borisenko <maximborisenko7@gmail.com>
2021-12-28 16:37:55 +0300

    Refactor profile related files

--- lib/presentation/pages/profile_page.dart
+++ lib/presentation/pages/profile_page.dart
@@ -2,6 +2,7 @@ import 'package:fire_chat/core/string_constants.dart';
 import 'package:fire_chat/presentation/blocs/theme_bloc/theme_bloc.dart';
 import 'package:fire_chat/presentation/blocs/theme_bloc/theme_bloc_state.dart';
 import 'package:fire_chat/presentation/blocs/theme_bloc/theme_events.dart';
+import 'package:fire_chat/presentation/widgets/profile_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
@@ -19,47 +20,6 @@ class ProfilePage extends StatelessWidget {
     context.read<ThemeBloc>().add(
           SwitchSystemThemeEvent(),
         );
-  }
-
-  Widget _profileCard(BuildContext context) {
-    return Card(
-      margin: const EdgeInsets.all(16),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(16),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          Icon(
-            Icons.person_pin,
-            size: 128,
-            color: Theme.of(context).primaryColor,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          const Text(
-            AppLocalization.username,
-            textAlign: TextAlign.center,
-          ),
-          const SizedBox(
-            height: 16,
-          ),
-          _placeholderContainer(context),
-          const SizedBox(
-            height: 16,
-          ),
-          _placeholderContainer(context),
-          const SizedBox(
-            height: 16,
-          ),
-          _placeholderContainer(context),
-          const SizedBox(
-            height: 16,
-          ),
-        ],
-      ),
-    );
   }
 
   Widget _systemThemeSettings(BuildContext context, ThemeBlocState state) {
@@ -83,18 +43,6 @@ class ProfilePage extends StatelessWidget {
           AppLocalization.useSystemThemeModeDescription,
         ),
       ],
-    );
-  }
-
-  Widget _placeholderContainer(BuildContext context) {
-    return Container(
-      color: Theme.of(context).primaryColor,
-      height: 16,
-      width: 64,
-      margin: const EdgeInsets.only(
-        left: 32,
-        right: 32,
-      ),
     );
   }
 
@@ -128,7 +76,7 @@ class ProfilePage extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                _profileCard(context),
+                const ProfileCard(),
                 _systemThemeSettings(context, state),
               ],
             ),
