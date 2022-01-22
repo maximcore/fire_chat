e403f53a149730a34c3f9bb47988d4fc043261bb
Maxim Borisenko <maximborisenko7@gmail.com>
2022-01-21 18:06:56 -0800

    Add firebase packages

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -1,9 +1,12 @@
 import 'package:fire_chat/config/flavors.dart';
 import 'package:fire_chat/fire_chat_app.dart';
 import 'package:fire_chat/injector.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   F.appFlavor = Flavor.DEV;
   await setup();
   runApp(
