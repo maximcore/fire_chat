455351b9c77647467aa40b8c160a832cb74894f4
Maxim Borisenko <maximborisenko7@gmail.com>
2022-01-31 22:48:43 +0300

    Update sign in row widget

--- lib/presentation/widgets/login_page/sign_in_credentials_row.dart
+++ lib/presentation/widgets/login_page/sign_in_credentials_row.dart
@@ -1,31 +1,56 @@
+import 'package:fire_chat/core/string_constants.dart';
 import 'package:fire_chat/presentation/blocs/auth_bloc/auth_bloc.dart';
+import 'package:fire_chat/presentation/widgets/common/custom_elevated_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-class SignInCredentialsRow extends StatelessWidget {
-  const SignInCredentialsRow({Key? key}) : super(key: key);
+class SignInCredentialsColumn extends StatelessWidget {
+  const SignInCredentialsColumn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        IconButton(
+        CustomElevatedButton(
           onPressed: () {
             context.read<AuthBloc>().loginWithGoogle();
           },
-          icon: const FaIcon(FontAwesomeIcons.google),
+          radius: 16,
+          padding: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              FaIcon(FontAwesomeIcons.google),
+              Text(AppLocalization.loginWithGoogleButtonText),
+              Opacity(
+                opacity: 0,
+                child: FaIcon(FontAwesomeIcons.google),
+              ),
+            ],
+          ),
         ),
-        IconButton(
+        const SizedBox(
+          height: 16,
+        ),
+        CustomElevatedButton(
           onPressed: () {
             context.read<AuthBloc>().loginWithFacebook();
           },
-          icon: const FaIcon(FontAwesomeIcons.facebook),
-        ),
-        const IconButton(
-          onPressed: null,
-          icon: FaIcon(FontAwesomeIcons.apple),
+          radius: 16,
+          padding: 50,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              FaIcon(FontAwesomeIcons.facebook),
+              Text(AppLocalization.loginWithFacebookButtonText),
+              Opacity(
+                opacity: 0,
+                child: FaIcon(FontAwesomeIcons.facebook),
+              ),
+            ],
+          ),
         ),
       ],
     );
