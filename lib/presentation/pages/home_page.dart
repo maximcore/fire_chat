189250248741db1b02b99695279ed3ce9fde6c2e
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-07 13:42:52 +0300

    Edit home page

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,14 +1,9 @@
 import 'package:fire_chat/config/routing/routes.dart';
 import 'package:fire_chat/core/string_constants.dart';
-import 'package:fire_chat/domain/repositories/posts_repository/posts_repository.dart';
-import 'package:fire_chat/presentation/blocs/posts_bloc/posts_bloc.dart';
-import 'package:fire_chat/presentation/blocs/posts_bloc/posts_events.dart';
 import 'package:fire_chat/presentation/pages/chats_page.dart';
 import 'package:fire_chat/presentation/pages/feed_page.dart';
 import 'package:fire_chat/presentation/widgets/common/flavors_banner.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:get_it/get_it.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -35,10 +30,18 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    final repository = GetIt.instance.get<PostsRepository>();
     return Stack(
       children: [
         Scaffold(
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Theme.of(context).primaryColor,
+            onPressed: () {
+              Navigator.of(context).pushNamed(
+                AppRoutes.addPostPage,
+              );
+            },
+            child: const Icon(Icons.add),
+          ),
           appBar: AppBar(
             automaticallyImplyLeading: false,
             title: Text(
@@ -57,13 +60,7 @@ class _HomePageState extends State<HomePage> {
             ],
             centerTitle: true,
           ),
-          body: BlocProvider<PostsBloc>(
-            create: (_) => PostsBloc(repository: repository)
-              ..add(
-                FetchingDataEvent(),
-              ),
-            child: _pages[_currentPageIndex],
-          ),
+          body: _pages[_currentPageIndex],
           bottomNavigationBar: BottomNavigationBar(
             currentIndex: _currentPageIndex,
             onTap: _onNavbarItemTap,
