f056cb134142a07c7b3134b237d00f25dac92cd6
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-07 13:42:41 +0300

    Add 'add post' page widget

---
+++ lib/presentation/pages/add_post_page.dart
@@ -0,0 +1,58 @@
+import 'package:fire_chat/core/string_constants.dart';
+import 'package:fire_chat/domain/entities/post_entity/post_entity.dart';
+import 'package:fire_chat/presentation/blocs/auth_bloc/auth_bloc.dart';
+import 'package:fire_chat/presentation/blocs/posts_bloc/posts_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AddPostPage extends StatelessWidget {
+  const AddPostPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AppLocalization.addPostPageTitle),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            addPostWidget(context),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget addPostWidget(BuildContext context) {
+    String description = '';
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: TextField(
+            onChanged: (String? value) {
+              description = value!;
+            },
+            maxLines: 5,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            final user = context.read<AuthBloc>().state.user!;
+            final post = PostEntity(
+              description: description,
+              username: user.email,
+            );
+            context.read<PostsBloc>().addPost(
+                  post: post,
+                  user: user,
+                );
+            Navigator.of(context).pop();
+          },
+          child: Text(AppLocalization.addPostText,),
+        ),
+      ],
+    );
+  }
+}
