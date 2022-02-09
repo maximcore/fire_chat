f3ffc5ff9238ffe0048c0a30d53305b26bbff40d
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-09 18:43:14 +0300

    Add function emulator

--- lib/config/emulator_suite.dart
+++ lib/config/emulator_suite.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 
@@ -17,4 +18,7 @@ Future connectToFirebaseEmulator() async {
   await FirebaseStorage.instance.useStorageEmulator(localHost, 9199);
 
   FirebaseFirestore.instance.useFirestoreEmulator(localHost, 8080);
+
+  FirebaseFunctions.instanceFor(region: 'europe-west3')
+      .useFunctionsEmulator(localHost, 5001);
 }
