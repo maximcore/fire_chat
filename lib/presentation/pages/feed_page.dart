a9fdb27697ba2e54eb3ee133f4290aed9676e76e
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-09 18:46:25 +0300

    Add like button

--- lib/presentation/pages/feed_page.dart
+++ lib/presentation/pages/feed_page.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:fire_chat/config/routing/routes.dart';
 import 'package:fire_chat/core/string_constants.dart';
+import 'package:fire_chat/presentation/blocs/auth_bloc/auth_bloc.dart';
 import 'package:fire_chat/presentation/blocs/posts_bloc/posts_bloc.dart';
 import 'package:fire_chat/presentation/blocs/posts_bloc/posts_bloc_state.dart';
 import 'package:fire_chat/presentation/blocs/posts_bloc/posts_events.dart';
@@ -46,6 +48,17 @@ class FeedPage extends StatelessWidget {
             if (index.isOdd) {
               _simulateError(context);
             }
+          },
+          onLikePressed: () async {
+            final callable = FirebaseFunctions.instanceFor(
+              region: 'europe-west3',
+            ).httpsCallable('likePost');
+            final user = context.read<AuthBloc>().state.user;
+            final res = await callable.call<Map>(<String, dynamic>{
+              'user': user?.id,
+              'postId': state.posts![index].postId,
+            });
+            //print(res.data);
           },
         );
       },
