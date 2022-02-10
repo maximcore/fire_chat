130c67205697bf66ba028f2e95c4f702316bf545
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-10 16:31:18 +0300

    Edit post widget

--- lib/presentation/widgets/post_widget.dart
+++ lib/presentation/widgets/post_widget.dart
@@ -21,22 +21,26 @@ class PostWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final user = context.read<AuthBloc>().state.user;
-    return GestureDetector(
-      onTap: onTap,
-      //onDoubleTap: onDoubleTap,
-      child: SizedBox(
-        height: 160,
-        child: Card(
-          margin: const EdgeInsets.only(top: 8, bottom: 8, left: 32, right: 32),
-          color: Theme.of(context).cardColor,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
+    final height = MediaQuery.of(context).size.height;
+    return SizedBox(
+      height: height / 2.5,
+      child: Card(
+        margin: const EdgeInsets.only(top: 8, bottom: 8, left: 32, right: 32),
+        color: Theme.of(context).cardColor,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                left: 12,
+                right: 12,
+                top: 6,
+              ),
+              //color: Colors.greenAccent,
+              child: Row(
                 children: [
                   CircleAvatar(
                     radius: 10,
@@ -44,16 +48,27 @@ class PostWidget extends StatelessWidget {
                       post.user.profilePictureUrl,
                     ).image,
                   ),
+                  const SizedBox(
+                    width: 6,
+                  ),
                   Text(
                     post.user.username,
                   ),
                 ],
               ),
-              Text(
-                post.description,
-                textAlign: TextAlign.center,
+            ),
+            Text(
+              post.description,
+              textAlign: TextAlign.center,
+            ),
+            Container(
+              padding: const EdgeInsets.only(
+                left: 12,
+                right: 12,
+                bottom: 6,
               ),
-              Row(
+              //color: Colors.deepPurpleAccent,
+              child: Row(
                 children: [
                   IconButton(
                     onPressed: onLikePressed,
@@ -64,10 +79,16 @@ class PostWidget extends StatelessWidget {
                         ? Colors.red
                         : Colors.white,
                   ),
+                  IconButton(
+                    onPressed: onTap,
+                    icon: const FaIcon(
+                      FontAwesomeIcons.comment,
+                    ),
+                  )
                 ],
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
