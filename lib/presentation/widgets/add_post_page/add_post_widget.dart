a7930accb3a3a3f26d75366243cd7f55a7ae4b43
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-10 16:32:16 +0300

    Refactor add post widget

---
+++ lib/presentation/widgets/add_post_page/add_post_widget.dart
@@ -0,0 +1,84 @@
+import 'dart:io';
+
+import 'package:file_picker/file_picker.dart';
+import 'package:fire_chat/core/string_constants.dart';
+import 'package:fire_chat/domain/entities/post_entity/post_entity.dart';
+import 'package:fire_chat/presentation/blocs/auth_bloc/auth_bloc.dart';
+import 'package:fire_chat/presentation/blocs/create_post_bloc/create_post_bloc.dart';
+import 'package:fire_chat/presentation/blocs/posts_bloc/posts_bloc.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AddPostWidget extends StatelessWidget {
+  const AddPostWidget({
+    Key? key,
+    required this.blocBuilderContext,
+  }) : super(key: key);
+
+  final BuildContext blocBuilderContext;
+
+  @override
+  Widget build(BuildContext context) {
+    final bloc = blocBuilderContext.read<CreatePostBloc>();
+    return Column(
+      children: [
+        const Text(AppLocalization.description),
+        Padding(
+          padding: const EdgeInsets.all(18),
+          child: TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+            ),
+            onChanged: (String? value) {
+              bloc.editDescription(value!);
+            },
+            maxLines: 5,
+          ),
+        ),
+        if (bloc.state.result?.files.single.path == null)
+          const SizedBox()
+        else
+          Image.file(
+            File(bloc.state.result?.files.single.path as String),
+            width: 100,
+            height: 150,
+          ),
+        ElevatedButton(
+          onPressed: () async {
+            final result = await FilePicker.platform.pickFiles(
+              type: FileType.custom,
+              allowedExtensions: [
+                'png',
+                'jpg',
+              ],
+            );
+            if (result != null) {
+              bloc.editImage(result);
+            }
+          },
+          child: const Text(
+            AppLocalization.uploadImageText,
+          ),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            final user = blocBuilderContext.read<AuthBloc>().state.user!;
+            final post = PostEntity(
+              description: bloc.state.description!,
+              user: user,
+            );
+            blocBuilderContext.read<PostsBloc>().addPost(
+                  post: post,
+                  user: user,
+                );
+            blocBuilderContext.read<CreatePostBloc>().savePost();
+            Navigator.of(blocBuilderContext).pop();
+          },
+          child: const Text(
+            AppLocalization.addPostText,
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/presentation/widgets/add_post_page/discard_post_alert_dialog.dart
@@ -0,0 +1,49 @@
+import 'package:fire_chat/core/string_constants.dart';
+import 'package:flutter/material.dart';
+
+class DiscardPostAlertDialog extends StatelessWidget {
+  const DiscardPostAlertDialog({
+    Key? key,
+    required this.context,
+    required this.builderContext,
+  }) : super(key: key);
+
+  final BuildContext context;
+  final BuildContext builderContext;
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text(
+        AppLocalization.discardPost,
+        style: TextStyle(),
+        textAlign: TextAlign.center,
+      ),
+      actions: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton(
+              onPressed: () {
+                Navigator.of(builderContext).pop();
+              },
+              child: Text(
+                AppLocalization.cancelText,
+                style: TextStyle(
+                  color: Theme.of(context).primaryColorLight,
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(builderContext).pop();
+                Navigator.of(context).pop();
+              },
+              child: const Text(AppLocalization.discardText),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
