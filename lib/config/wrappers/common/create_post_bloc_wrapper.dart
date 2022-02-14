0b9f8abcb258b74a6c3b7cd47d32a6132b86009f
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-14 17:37:31 +0300

    Pass repositories to create post bloc

--- lib/config/wrappers/common/create_post_bloc_wrapper.dart
+++ lib/config/wrappers/common/create_post_bloc_wrapper.dart
@@ -1,6 +1,9 @@
+import 'package:fire_chat/domain/repositories/posts_repository/posts_repository.dart';
+import 'package:fire_chat/domain/repositories/storage_repository/storage_repository.dart';
 import 'package:fire_chat/presentation/blocs/create_post_bloc/create_post_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
 
 class CreatePostBlocWrapper extends StatelessWidget {
   const CreatePostBlocWrapper({
@@ -12,8 +15,13 @@ class CreatePostBlocWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final postsRepository = GetIt.instance.get<PostsRepository>();
+    final storageRepository = GetIt.instance.get<StorageRepository>();
     return BlocProvider<CreatePostBloc>(
-      create: (_) => CreatePostBloc(),
+      create: (_) => CreatePostBloc(
+        postsRepository: postsRepository,
+        storageRepository: storageRepository,
+      ),
       child: child,
     );
   }
