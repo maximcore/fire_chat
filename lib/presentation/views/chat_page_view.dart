65203d8a8fdb53a78bf569e0e2d74f5f96185dc3
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-18 14:05:34 +0300

    Move chat page to mvvm

---
+++ lib/presentation/views/chat_page_view.dart
@@ -0,0 +1,17 @@
+import 'package:fire_chat/core/string_constants.dart';
+import 'package:flutter/material.dart';
+
+class ChatPageView extends StatelessWidget {
+  const ChatPageView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(AppLocalization.chatsPageTitle),
+        centerTitle: true,
+      ),
+      body: ListView(),
+    );
+  }
+}
