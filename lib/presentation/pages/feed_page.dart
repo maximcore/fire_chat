2add9acb622c0711ccf8054f83ecfdd03b1daddf
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-18 14:49:11 +0300

    Move feed page to mvvm

--- lib/presentation/pages/feed_page.dart
+++ lib/presentation/pages/feed_page.dart
@@ -1,39 +1,18 @@
-import 'package:fire_chat/presentation/blocs/create_post_bloc/create_post_bloc.dart';
-import 'package:fire_chat/presentation/blocs/create_post_bloc/create_post_bloc_state.dart';
 import 'package:fire_chat/presentation/blocs/posts_bloc/posts_bloc.dart';
-import 'package:fire_chat/presentation/blocs/posts_bloc/posts_bloc_state.dart';
-import 'package:fire_chat/presentation/widgets/feed_page/error_banner.dart';
-import 'package:fire_chat/presentation/widgets/feed_page/posts_list.dart';
+import 'package:fire_chat/presentation/views/feed_page_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class FeedPage extends StatelessWidget {
   const FeedPage({Key? key}) : super(key: key);
 
+  void fetchPosts(BuildContext context) =>
+      context.read<PostsBloc>().fetchPosts();
+
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer<PostsBloc, PostsBlocState>(
-      listener: (context, state) {
-        if (context.read<CreatePostBloc>().state.status ==
-            CreatePostBlocStatus.ready) {
-          context.read<PostsBloc>().fetchPosts();
-        }
-      },
-      builder: (_, state) {
-        switch (state.status) {
-          case PostsBlocStatus.initial:
-          case PostsBlocStatus.loading:
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          case PostsBlocStatus.ready:
-            return PostsList(
-              state: state,
-            );
-          case PostsBlocStatus.error:
-            return const ErrorBanner();
-        }
-      },
+    return FeedPageView(
+      fetchPosts: () => fetchPosts(context),
     );
   }
 }
---
+++ lib/presentation/views/feed_page_view.dart
@@ -0,0 +1,44 @@
+import 'package:fire_chat/presentation/blocs/create_post_bloc/create_post_bloc.dart';
+import 'package:fire_chat/presentation/blocs/create_post_bloc/create_post_bloc_state.dart';
+import 'package:fire_chat/presentation/blocs/posts_bloc/posts_bloc.dart';
+import 'package:fire_chat/presentation/blocs/posts_bloc/posts_bloc_state.dart';
+import 'package:fire_chat/presentation/widgets/feed_page/error_banner.dart';
+import 'package:fire_chat/presentation/widgets/feed_page/posts_list.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class FeedPageView extends StatelessWidget {
+  const FeedPageView({
+    Key? key,
+    required this.fetchPosts,
+  }) : super(key: key);
+
+  final Function() fetchPosts;
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<PostsBloc, PostsBlocState>(
+      listener: (context, state) {
+        if (context.read<CreatePostBloc>().state.status ==
+            CreatePostBlocStatus.ready) {
+          fetchPosts();
+        }
+      },
+      builder: (_, state) {
+        switch (state.status) {
+          case PostsBlocStatus.initial:
+          case PostsBlocStatus.loading:
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          case PostsBlocStatus.ready:
+            return PostsList(
+              state: state,
+            );
+          case PostsBlocStatus.error:
+            return const ErrorBanner();
+        }
+      },
+    );
+  }
+}
