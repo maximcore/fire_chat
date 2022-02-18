8f4399e6b4db332081d9ec0c0bf56b78c786960d
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-18 14:44:24 +0300

    Move home page to mvvm

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -1,65 +1,36 @@
 import 'package:fire_chat/config/routing/routes.dart';
-import 'package:fire_chat/core/string_constants.dart';
-import 'package:fire_chat/presentation/blocs/create_post_bloc/create_post_bloc.dart';
-import 'package:fire_chat/presentation/blocs/create_post_bloc/create_post_bloc_state.dart';
 import 'package:fire_chat/presentation/blocs/posts_bloc/posts_bloc.dart';
-import 'package:fire_chat/presentation/pages/feed_page.dart';
-import 'package:fire_chat/presentation/widgets/common/flavors_banner.dart';
+import 'package:fire_chat/presentation/views/home_page_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  void fetchPosts(BuildContext context) =>
+      context.read<PostsBloc>().fetchPosts();
+
+  void goToChats(BuildContext context) => Navigator.pushNamed(
+        context,
+        AppRoutes.chatPageRoute,
+      );
+
+  void goToProfile(BuildContext context) => Navigator.pushNamed(
+        context,
+        AppRoutes.profilePageRoute,
+      );
+
+  void goToAddPostPage(BuildContext context) => Navigator.of(context).pushNamed(
+        AppRoutes.addPostPage,
+      );
+
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Scaffold(
-          floatingActionButton: FloatingActionButton(
-            backgroundColor: Theme.of(context).primaryColor,
-            onPressed: () {
-              Navigator.of(context).pushNamed(
-                AppRoutes.addPostPage,
-              );
-            },
-            child: const Icon(Icons.add),
-          ),
-          appBar: AppBar(
-            automaticallyImplyLeading: false,
-            title: const Text(AppLocalization.feedPageTitle),
-            actions: [
-              IconButton(
-                icon: const FaIcon(FontAwesomeIcons.facebookMessenger),
-                onPressed: () => Navigator.pushNamed(
-                  context,
-                  AppRoutes.chatPageRoute,
-                ),
-              ),
-              IconButton(
-                icon: const Icon(Icons.account_circle_rounded),
-                onPressed: () => Navigator.pushNamed(
-                  context,
-                  AppRoutes.profilePageRoute,
-                ),
-              ),
-            ],
-            centerTitle: true,
-          ),
-          body: BlocConsumer<CreatePostBloc, CreatePostBlocState>(
-            listener: (context, state) {
-              if (state.status == CreatePostBlocStatus.ready) {
-                context.read<PostsBloc>().fetchPosts();
-              }
-            },
-            builder: (context, state) {
-              return const FeedPage();
-            },
-          ),
-        ),
-        const FlavorsBanner(),
-      ],
+    return HomePageView(
+      fetchPosts: () => fetchPosts(context),
+      goToChats: () => goToChats(context),
+      goToProfile: () => goToProfile(context),
+      goToAddPostPage: () => goToAddPostPage(context),
     );
   }
 }
---
+++ lib/presentation/views/home_page_view.dart
@@ -0,0 +1,64 @@
+import 'package:fire_chat/core/string_constants.dart';
+import 'package:fire_chat/presentation/blocs/create_post_bloc/create_post_bloc.dart';
+import 'package:fire_chat/presentation/blocs/create_post_bloc/create_post_bloc_state.dart';
+import 'package:fire_chat/presentation/pages/feed_page.dart';
+import 'package:fire_chat/presentation/widgets/common/flavors_banner.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomePageView extends StatelessWidget {
+  const HomePageView({
+    Key? key,
+    required this.fetchPosts,
+    required this.goToChats,
+    required this.goToProfile,
+    required this.goToAddPostPage,
+  }) : super(key: key);
+
+  final Function() fetchPosts;
+  final VoidCallback goToChats;
+  final VoidCallback goToProfile;
+  final VoidCallback goToAddPostPage;
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        Scaffold(
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Theme.of(context).primaryColor,
+            onPressed: goToAddPostPage,
+            child: const Icon(Icons.add),
+          ),
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            title: const Text(AppLocalization.feedPageTitle),
+            actions: [
+              IconButton(
+                icon: const FaIcon(FontAwesomeIcons.facebookMessenger),
+                onPressed: goToChats,
+              ),
+              IconButton(
+                icon: const Icon(Icons.account_circle_rounded),
+                onPressed: goToProfile,
+              ),
+            ],
+            centerTitle: true,
+          ),
+          body: BlocConsumer<CreatePostBloc, CreatePostBlocState>(
+            listener: (context, state) {
+              if (state.status == CreatePostBlocStatus.ready) {
+                fetchPosts();
+              }
+            },
+            builder: (context, state) {
+              return const FeedPage();
+            },
+          ),
+        ),
+        const FlavorsBanner(),
+      ],
+    );
+  }
+}
