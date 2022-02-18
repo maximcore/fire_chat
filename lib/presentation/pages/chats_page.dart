39880a095e1949ec326d8d9048197a8e24341bd3
Maxim Borisenko <maximborisenko7@gmail.com>
2022-02-18 14:03:45 +0300

    Move chats page to mvvm

--- lib/presentation/pages/chats_page.dart
+++ lib/presentation/pages/chats_page.dart
@@ -1,26 +1,17 @@
 import 'package:fire_chat/config/routing/routes.dart';
-import 'package:fire_chat/domain/entities/chat_entity/chat_entity.dart';
-import 'package:fire_chat/presentation/widgets/chat_widget.dart';
+import 'package:fire_chat/presentation/views/chats_page_view.dart';
 import 'package:flutter/material.dart';
 
 class ChatsPage extends StatelessWidget {
   const ChatsPage({Key? key}) : super(key: key);
 
-  void _goToChat(BuildContext context) =>
+  void goToChat(BuildContext context) =>
       Navigator.pushNamed(context, AppRoutes.chatPageRoute);
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 20,
-      itemBuilder: (context, index) {
-        return ChatWidget(
-          chat: ChatEntity(userId: index),
-          onTap: () {
-            _goToChat(context);
-          },
-        );
-      },
+    return ChatsPageView(
+      onTap: () => goToChat(context),
     );
   }
 }
