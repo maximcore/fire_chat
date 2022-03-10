a4042098d2346acf8f88d3346128eafcc89c6377
Maxim Borisenko <maximborisenko7@gmail.com>
2022-03-10 18:31:19 +0300

    Edit chat page title

--- lib/presentation/views/chat_page_view.dart
+++ lib/presentation/views/chat_page_view.dart
@@ -8,7 +8,7 @@ class ChatPageView extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(AppLocalization.chatsPageTitle),
+        title: const Text(AppLocalization.chatPageTitle),
         centerTitle: true,
       ),
       body: ListView(),
